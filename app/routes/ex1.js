76c8d6156b3d90a5c1961a9dd5fdf945bc8cc6ef
pelgaetan <gaetanpell@hotmail.fr>
2019-01-30 14:57:12 +0100

    premier git

---
+++ app/routes/ex1.js
@@ -0,0 +1,25 @@
+import Route from '@ember/routing/route';
+import EmberObject, {computed} from '@ember/object';
+const Note=EmberObject.extend({
+  content: 'Aucun',
+  MAX: 100,
+  size : computed('content',function(){
+    return this.MAX-this.content.length;
+  }),
+
+  style: computed('size',function(){
+    if(this.size<20){
+      return "danger";
+    }else if(this.size<50){
+      return "warning";
+    }else{
+      return "info";
+    }
+  })
+  }
+);
+export default Route.extend({
+model(){
+  return Note.create();
+}
+});
