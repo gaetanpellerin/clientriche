829dce291c051e5a271bbf6b54324fb4aa223491
pelgaetan <gaetanpell@hotmail.fr>
2019-01-30 15:44:51 +0100

    deuxieme import

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions:{
+    save(note){
+      //let model=this.modelFor(this.routeName);
+      if(note.get('content')){
+        note.set('info','Note Sauvegardée !');
+      }
+      }
+
+
+  }
 });
